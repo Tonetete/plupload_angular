13de4d5f3b726dfe44ad7296086733d4252ff5fb
Tone <antoniorivasfuentes@gmail.com>
2016-01-25 21:54:59 +0100

    Init yeoman project.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name puploadAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the puploadAngularApp
+ */
+angular.module('puploadAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name puploadAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the puploadAngularApp
+ */
+angular.module('puploadAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
