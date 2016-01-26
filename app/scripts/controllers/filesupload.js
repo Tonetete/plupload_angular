ab7716d5ec29fc985725b8cfac0bce8dfe84a0f6
Tone <antoniorivasfuentes@gmail.com>
2016-01-26 22:31:05 +0100

    Resolve problem with updating progress cause for "plupload.formatSize(file.size)"

--- app/scripts/controllers/filesupload.js
+++ app/scripts/controllers/filesupload.js
@@ -17,16 +17,17 @@ angular.module('puploadAngularApp')
      browse_button : 'pickfiles', // you can pass an id...
      container: document.getElementById('container'), // ... or DOM Element itself
      url : 'http://localhost/uploads/upload.php',
-     flash_swf_url : '../js/Moxie.swf',
-     silverlight_xap_url : '../js/Moxie.xap',
+     flash_swf_url : '../plupload/Moxie.swf',
+     silverlight_xap_url : '../plupload/Moxie.xap',
 
-     chunk_size: '5000kb',
+     chunk_size: '50000kb',
 
      filters : {
        max_file_size : '10000mb',
        mime_types: [
          {title : "Image files", extensions : "jpg,gif,png"},
-         {title : "Zip files", extensions : "avi,mp4,wmv,zip"},
+         {title : "Zip files", extensions : "zip"},
+         {title : "Video files", extensions : "avi,mp4,wmv,mkv"},
          {title : "PDF files", extensions : "pdf"}
        ]
      },
@@ -39,32 +40,32 @@ angular.module('puploadAngularApp')
          //document.getElementById('filelist').innerHTML = '';
          $scope.uploadFiles = function(){
            $scope.uploader.start();
-           $scope.showProgress=true;
+           //$scope.showProgress=true;
            return false;
          };
        },
 
        FilesAdded: function(up, files) {
          plupload.each(files, function(file) {
-           file.size = plupload.formatSize(file.size);
+           //file.size = plupload.formatSize(file.size); ----> CULPABLE!!!!!!!
            $scope.filesUploads.push(file);
            $scope.$apply(); // probably plupload is blocking the scope apply event so we have to aply ourselves.
            console.log(file);
            //document.getElementById('filelist').innerHTML += '<div id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></div>';
          });
        },
-       QueueChanged: function(up){
-         console.log(up);
-       },
 
        UploadProgress: function(up, file) {
-         $scope.progressFile = file.percent;
+         //$scope.progressFile = file.percent;
+         console.log(file.percent);
+         $scope.progressFile = (file.loaded / file.origSize) * 100;
+         $scope.$apply();
+         //console.log("progress: "+$scope.progressFile);
          //document.getElementById(file.id).getElementsByTagName('b')[0].innerHTML = '<span>' + file.percent + "%</span>";
        },
 
        Error: function(up, err) {
          console.log("Error #" + err.code + ": " + err.message);
-         console.log(err);
          //document.getElementById('console').appendChild(document.createTextNode("\nError #" + err.code + ": " + err.message));
        }
      }
